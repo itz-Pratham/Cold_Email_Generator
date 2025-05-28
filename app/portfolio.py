5165caa6ffefcc5e80471da8b84a2d1bafeabc28
Pratham Mittal <prathammittal355@gmail.com>
2025-05-28 12:23:51 +0530

    portfolio changes

--- app/portfolio.py
+++ app/portfolio.py
@@ -7,7 +7,7 @@ class Portfolio:
     def __init__(self, file_path="app/data/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.chroma_client = chromadb.PersistentClient(path='vectorstore')
         self.collection = self.chroma_client.get_or_create_collection(
             name="portfolio",
             embedding_function=DefaultEmbeddingFunction()
