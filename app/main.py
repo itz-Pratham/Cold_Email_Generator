c435649b18724c1cabbd0445eca1374772087ece
Pratham Mittal <prathammittal355@gmail.com>
2025-06-01 01:26:59 +0530

    custom_input_toggling_added

--- app/main.py
+++ app/main.py
@@ -5,10 +5,49 @@ from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
 
+Default_person_info = '''You are Pratham Mittal, a passionate and skilled software and machine learning developer based in New Delhi. 
+                        With a strong foundation in Computer Science (CGPA: 9.11), hands-on experience at DRDO as a Machine Learning Intern, 
+                        and over 600 DSA problems solved, you bring robust analytical and coding capabilities to the table.'''
+
+Default_proj_info = '''You’ve built and deployed multiple impactful projects, including:
+            - An Amazon Review Sentiment Analyzer with a Streamlit UI and 93.49% accuracy,
+            - A Transaction Analyzer with visual breakdowns for financial insights,
+            - EventVerse, a full-stack React + Firebase event listing app,
+            - Cold Mail Generator with toggling system and Geneative AI.
+            '''
+
 
 def create_streamlit_app(llm, portfolio, clean_text):
     st.title("📧 Cold Mail Generator")
     url_input = st.text_input("Enter a URL:", value="https://careers.nike.com/software-engineer/job/R-61132")
+
+
+
+
+
+
+    use_defaults = st.toggle("Use Pratham's default info", value=True)
+
+    # User inputs
+    if use_defaults:
+        person_info = Default_person_info
+        project_desc = Default_proj_info
+    else:
+        person_info = st.text_area("Enter your personal bio:", height=150)
+        project_desc = st.text_area("Enter your project description:", height=150)
+
+        # Upload Portfolio CSV
+        uploaded_file = st.file_uploader("Upload your portfolio.csv", type=["csv"])
+        if uploaded_file is not None:
+            with open("assets/portfolio.csv", "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            st.success("Portfolio updated successfully!")
+
+
+
+
+
+
     submit_button = st.button("Submit")
 
     if submit_button:
@@ -20,7 +59,7 @@ def create_streamlit_app(llm, portfolio, clean_text):
             for job in jobs:
                 skills = job.get('skills', [])
                 links = portfolio.query_links(skills)
-                email = llm.write_mail(job, links)
+                email = llm.write_mail(job, links, person_info, project_desc)
                 st.code(email, language='markdown')
         except Exception as e:
             import traceback
