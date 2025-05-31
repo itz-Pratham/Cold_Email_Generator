e4e64a9baad7f7aa04ce10d52853d7f797ae01fc
Pratham Mittal <prathammittal355@gmail.com>
2025-06-01 00:37:29 +0530

    updated_portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import chromadb
 import uuid
-from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
+
 
 class Portfolio:
     def __init__(self, file_path="app/data/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient(path='vectorstore')
-        self.collection = self.chroma_client.get_or_create_collection(
-            name="portfolio",
-            embedding_function=DefaultEmbeddingFunction()
-        )
+        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
 
     def load_portfolio(self):
